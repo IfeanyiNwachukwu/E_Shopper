ad9fcffb2002632590dfd5f210345d8dd7c027a9
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-05-05 15:19:22 +0100

    Added Entity Framework core nuget packages and DbContect class

---
+++ EShopperSolution/WEB/EShopper.Web/Infrastructure/EShopperContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EShopper.Web.Infrastructure
+{
+    public class EShopperContext : DbContext
+    {
+        public EShopperContext(DbContextOptions<EShopperContext> options):base(options)
+        {
+
+        }
+    }
+}
